3b40efde471fa5126db397fc03f7fa2c859226cb
z347 <shulo.vova@gmail.com>
2020-09-06 18:11:57 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# About this project
+
+This is a simple browser game on JavaScript. The goal is to score as many points as possible in a period of time. You can choose the duration of the game and after its completion you will see the number of points scored.
+
+## Demo
+
+![Farmers Market Finder - Animated gif demo](demo/demo.gif)
+
+## Run Project Locally
+
+1. Clone or fork this repository.
+2. Open file index.html in browser.
+
+## Technologies Used
+
+- JavaScript
+- ES6 +
+- CSS3
+- HTML5
---
+++ index.js
@@ -0,0 +1,116 @@
+let $start = document.querySelector('#start');
+let $game = document.querySelector('#game');
+let $time = document.querySelector('#time');
+let $result = document.querySelector('#result');
+let $timeHeader = document.querySelector('#time-header');
+let $resultHeader = document.querySelector('#result-header');
+let $gameTime = document.querySelector('#game-time');
+
+let score = 0;
+let isGameStarted = false;
+let colors = [
+    '#CB356B',
+    '#BD3F32',
+    '#3A1C71',
+    '#D76D77',
+    '#283c86',
+    '#45a247',
+    '#8e44ad',
+    '#155799',
+    '#159957',
+    '#000046',
+    '#1CB5E0',
+    '#2F80ED',
+];
+
+$start.addEventListener('click', startGame);
+$game.addEventListener('click', handleBoxClick);
+$gameTime.addEventListener('input', setGameTime);
+
+function show($el) {
+    $el.classList.remove('hide');
+}
+
+function hide($el) {
+    $el.classList.add('hide');
+}
+
+function startGame() {
+    score = 0;
+    setGameTime();
+    $gameTime.setAttribute('disabled', 'true');
+    isGameStarted = true;
+    $game.style.backgroundColor = '#fff';
+    hide($start);
+
+    let interval = setInterval(function() {
+        let time = parseFloat($time.textContent);
+
+        if (time <= 0) {
+            clearInterval(interval);
+            endGame();
+        } else {
+            $time.textContent = (time - 0.1).toFixed(1);
+        }
+    }, 100);
+
+    renderBox();
+}
+
+function setGameScore() {
+    $result.textContent = score.toString();
+}
+
+function setGameTime() {
+    let time = +$gameTime.value;
+    $time.textContent = time.toFixed(1);
+    show($timeHeader);
+    hide($resultHeader);
+}
+
+function endGame() {
+    isGameStarted = false;
+    setGameScore();
+    $gameTime.removeAttribute('disabled');
+    show($start);
+    $game.innerHTML = '';
+    $game.style.backgroundColor = '#ccc';
+    hide($timeHeader);
+    show($resultHeader);
+}
+
+function handleBoxClick(event) {
+    if (!isGameStarted) {
+        return;
+    }
+
+    if (event.target.dataset.box) {
+        score++;
+        renderBox();
+    }
+}
+
+function renderBox() {
+    $game.innerHTML = '';
+    let box = document.createElement('div');
+    let boxSize = getRandom(30, 100);
+    let gameSize = $game.getBoundingClientRect();
+    let maxTop = gameSize.height - boxSize;
+    let maxLeft = gameSize.width - boxSize;
+
+    let randomColorIndex = getRandom(0, colors.length);
+
+    box.style.height = box.style.width = boxSize + 'px';
+    box.style.position = 'absolute';
+    box.style.backgroundColor = colors[randomColorIndex];
+    box.style.top = getRandom(0, maxTop) + 'px';
+    box.style.left = getRandom(0, maxLeft) + 'px';
+    box.style.cursor = 'pointer';
+    box.setAttribute('data-box', 'true');
+
+    $game.insertAdjacentElement('afterbegin', box);
+}
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
